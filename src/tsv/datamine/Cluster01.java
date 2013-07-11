87375a1986f979d3743a1ea003e6e25968ab0995
Rado Tsvetkov <radoslav.tsvetkov@cortalconsors.de>
2013-07-11 09:38:35 +0200

    initial commit

---
+++ src/tsv/datamine/Cluster01.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tsv.datamine;
+
+/**
+ *
+ * @author cs8170
+ */
+public class Cluster01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
